ea6f5edb937a5df6e5e0827af85b85ed6f8f5906
Tạ Đức Quang <45884093+taducquang@users.noreply.github.com>
2026-04-28 05:33:41 +0700

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -26,8 +26,5 @@ export default defineConfig({
       react: resolve(__dirname, 'node_modules/react'),
       'react-dom': resolve(__dirname, 'node_modules/react-dom'),
     },
-  },
-  rollupOptions: {
-    external: ['@features/recent-projects/main']
-  },  
+  }
 });
